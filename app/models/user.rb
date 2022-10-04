3c8d374879517337f5eb7ae197d4766ba72a775d
hayashiyukino <112701375+hayashiyukino@users.noreply.github.com>
2022-10-04 17:33:25 +0000

    [Start]応用４

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,12 +3,29 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-
+  # bookモデルに対してのアソシエーション
   has_many :books, dependent: :destroy
+  # いいねモデルに対して
   has_many :favorites, dependent: :destroy
+  # コメントに対して
   has_many :book_comments, dependent: :destroy
+  
+  # フォローをした
+  #  relationships = 分かりやすいように名前をつけただけ
+  #  class_name: "Relationship"でRelationshipテーブルを参照
+  #  foreign_key(外侮キー)で参照するカラムを指定
+  has_many :relationships, class_name: "Relationship", foreign_key: "follower_id", dependent: :destroy
+  # フォローされた
+  #  reverse_of_relationships = 分かりやすいように名前をつけただけ
+  has_many :reverse_of_relationships, class_name: "Relationship", foreign_key: "followed_id", dependent: :destroy
   has_one_attached :profile_image
-
+  # 一覧画面で使う
+  #  フォロー・フォロワーの一覧画面で、user.followersという記述でフォロワーを表示したいので、
+  #  throughでスルーするテーブル、sourceで参照するカラムを指定。
+  has_many :followings, through: :relationships, source: :followed
+  has_many :followers, through: :reverse_of_relationships, source: :follower
+  
+  
   validates :name, length: { minimum: 2, maximum: 20 }, uniqueness: true
   validates :introduction, length: { maximum: 50 }
   
@@ -21,5 +38,18 @@ class User < ApplicationRecord
   
   def get_profile_image
     (profile_image.attached?) ? profile_image : 'no_image.jpg'
+  end
+  
+  # フォローしたときの処理の定義
+  def follow(user_id)
+    relationships.create(followed_id: user_id)
+  end
+  # フォローを外すときの処理の定義
+  def unfollow(user_id)
+    relationships.find_by(followed_id: user_id).destroy
+  end
+  # フォローしているか判定
+  def following?(user)
+    followings.include?(user)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,24 @@
 Rails.application.routes.draw do
+  get 'relationships/followings'
+  get 'relationships/followers'
   # get 'book_comments create/destroy'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   devise_for :users
 
   root :to =>"homes#top"
   get "home/about"=>"homes#about"
-
+  
+  # ネストさせる
   resources :books, only: [:index,:show,:edit,:create,:destroy,:update] do
     resources:book_comments,only:[:create,:destroy]
   end
-  resources :users, only: [:index,:show,:edit,:update]
+  
+  # ネストさせる
+  resources :users, only: [:index,:show,:edit,:update] do
+    resource :relationships, only: [:create, :destroy]
+    get 'followings' => 'relationships#followings', as: 'followings'
+    get 'followers' => 'relationships#followers', as: 'followers'
+  end
 
   post 'favorite/:id' => 'favorites#create', as: 'create_favorite'
   delete 'favorite/:id' => 'favorites#destroy', as: 'destroy_favorite'
