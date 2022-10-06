018bd5f68b14ad376d856185f9177a4c425e0f7a
hayashiyukino <112701375+hayashiyukino@users.noreply.github.com>
2022-10-06 18:43:21 +0000

    [Update]いいね非同期途中

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,23 +1,23 @@
 class FavoritesController < ApplicationController
+  #「before_action」を使用して、投稿のidを取得できるようにする
+  before_action :book_params
 
   # createアクション(いいねを押す機能)を定義
   def create
     Favorite.create(user_id: current_user.id, book_id: params[:id])
-    redirect_to request.referer
   end
 
   # destroyアクション(いいねを解除する機能)を定義
   def destroy
     Favorite.find_by(user_id: current_user.id, book_id: params[:id]).destroy
-    redirect_to request.referer
   end
   
-  def index
-    # @booksにBookテーブルに保存されたデータを全取得する
-    @books = Book.all
-    @user = current_user
-    # @bookに空のモデルオブジェクトBookを作成し代入する
-    @book = Book.new
+  # 非同期処理で必要
+  # 取得した投稿idを保存する
+  private
+
+  def book_params
+    @book = Book.find(params[:id])
   end
   
 end
--- app/models/favorite.rb
+++ app/models/favorite.rb
@@ -1,4 +1,7 @@
 class Favorite < ApplicationRecord
   belongs_to :user  # この1行を追加
   belongs_to :book  # この1行を追加
+  
+  # 1回以上は押せない様にバリデーションを設定します。
+  validates :user_id, uniqueness: { scope: :book_id }
 end
